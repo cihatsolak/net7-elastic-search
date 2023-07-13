4e9448a63dfa91fabccf4770d383d2dfca0646c9
Cihat Solak <cihatsolak@hotmail.com>
2023-07-13 23:08:32 +0300

    Create classes product and product feature

---
+++ ElasticSearch.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ElasticSearch.API.Models
+{
+    public class Product
+    {
+        //Elastic Search Auto Id. Elastic search tarafından gerçekten bir id olarak tutulmasını istiyorsak
+        //Elastic tarafından primary key görevi görsün
+        [PropertyName("_id")] 
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public ProductFeature Feature { get; set; }
+    }
+}
