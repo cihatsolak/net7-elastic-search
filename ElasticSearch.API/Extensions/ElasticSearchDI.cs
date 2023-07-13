66880865c8e2edd0c2a6cdb26855de1ac7e60a41
Cihat Solak <cihatsolak@hotmail.com>
2023-07-13 22:41:27 +0300

    create elastic client class

---
+++ ElasticSearch.API/Extensions/ElasticSearchDI.cs
@@ -0,0 +1,19 @@
+﻿namespace ElasticSearch.API.Extensions;
+
+public static class ElasticSearchDI
+{
+    public static void AddCustomElasticSearch(this IServiceCollection services, IConfiguration configuration)
+    {
+        ElasticSetting elasticSetting = configuration.GetSection("ElasticSetting").Get<ElasticSetting>();
+
+        //Gerçek hayatta birden fazla node ile çalışılabilir ama docker da tek node çalışıyoruz.
+        var singleNodeConnectionPool = new SingleNodeConnectionPool(new Uri(elasticSetting.Uri));
+        var connectionString = new ConnectionSettings(singleNodeConnectionPool);
+
+        connectionString = connectionString.BasicAuthentication(elasticSetting.Username, elasticSetting.Password);
+
+        var elasticClient = new ElasticClient(connectionString);
+
+        services.AddSingleton(elasticClient);
+    }
+}
---
+++ ElasticSearch.API/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using Elasticsearch.Net;
+global using ElasticSearch.API.Extensions;
+global using ElasticSearch.API.Settings;
+global using Nest;
