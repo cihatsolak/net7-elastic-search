b24beb7b9d1f534af6af6dc40a24a5d7cd4a7143
Cihat Solak <cihatsolak@hotmail.com>
2023-07-19 14:22:37 +0300

    Create blog search method

--- ElasticSearch.Web.Blog/Repositories/BlogRepository.cs
+++ ElasticSearch.Web.Blog/Repositories/BlogRepository.cs
@@ -24,4 +24,26 @@ public sealed class BlogRepository
 
         return blog;
     }
+
+    public async Task<List<Blog>> SearchAsync(string searchText)
+    {
+        var result = await _elasticsearchClient.SearchAsync<Blog>(search => search.Index(BLOG_INDEX_NAME)
+            .Size(3)
+                .Query(query => query
+                    .Bool(b => b
+                        .Should(
+                            should => should.Match(match => match.Field(field => field.Content).Query(searchText)),
+                            should => should.MatchBoolPrefix(matchBoolPrefix => matchBoolPrefix.Field(field => field.Title).Query(searchText))
+                                )
+                         )
+                      )
+             );
+
+        foreach (var hit in result.Hits) 
+        {
+            hit.Source.Id = hit.Id;
+        }
+
+        return result.Documents.ToList();
+    }
 }
