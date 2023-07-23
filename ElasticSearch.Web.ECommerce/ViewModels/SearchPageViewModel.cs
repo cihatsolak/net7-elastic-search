6caeb37708a686cf03712c05b4dba5c9c8fcf055
Cihat Solak <cihatsolak@hotmail.com>
2023-07-23 18:35:16 +0300

    Pagination method for search page occurs

--- ElasticSearch.Web.ECommerce/ViewModels/SearchPageViewModel.cs
+++ ElasticSearch.Web.ECommerce/ViewModels/SearchPageViewModel.cs
@@ -2,28 +2,38 @@
 
 public class SearchPageViewModel
 {
-	public long TotalCount { get; set; }
-	public int Page { get; set; } = 1;
-	public int PageSize { get; set; } = 10;
-	public long PageLinkCount { get; set; }
-	public List<ECommerceViewModel> List { get; set; }
-	public ECommerceSearchViewModel SearchViewModel { get; set; }
+    public long TotalCount { get; set; }
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public long PageLinkCount { get; set; }
+    public List<ECommerceViewModel> List { get; set; }
+    public ECommerceSearchViewModel SearchViewModel { get; set; }
 
-	public string CreatePageUrl(HttpRequest request, int page, int pageSize)
-	{
-		string currentUrl = new Uri($"{request.Scheme}://{request.Host}{request.Path}{request.QueryString}").AbsoluteUri;
+    public string CreatePageUrl(HttpRequest request, long page, int pageSize)
+    {
+        string currentUrl = new Uri($"{request.Scheme}://{request.Host}{request.Path}{request.QueryString}").AbsoluteUri;
 
-		if (currentUrl.Contains("Page", StringComparison.OrdinalIgnoreCase))
-		{
-			currentUrl = currentUrl.Replace($"Page={Page}", $"Page={page}", StringComparison.OrdinalIgnoreCase);
+        if (currentUrl.Contains("Page", StringComparison.OrdinalIgnoreCase))
+        {
+            currentUrl = currentUrl.Replace($"Page={Page}", $"Page={page}", StringComparison.OrdinalIgnoreCase);
 
-			currentUrl = currentUrl.Replace($"PageSize={Page}", $"PageSize={page}", StringComparison.OrdinalIgnoreCase);
-		}
-		else
-		{
-			currentUrl = $"{currentUrl}?Page={page}&PageSize={pageSize}";
-		}
+            currentUrl = currentUrl.Replace($"PageSize={Page}", $"PageSize={page}", StringComparison.OrdinalIgnoreCase);
+        }
+        else
+        {
+            currentUrl = $"{currentUrl}?Page={page}&PageSize={pageSize}";
+        }
 
-		return currentUrl;
-	}
+        return currentUrl;
+    }
+
+    public int StartPage()
+    {
+        return Page - 6 <= 0 ? 1 : Page - 6;
+    }
+
+    public long EndPage()
+    {
+        return Page + 6 >= PageLinkCount ? PageLinkCount : Page + 6;
+    }
 }
